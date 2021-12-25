1eea7e63dcf41bfc78005aab0854b9b62850b2f5
xemeds <xemeds@protonmail.com>
2021-12-25 22:27:06 +0300

    Added the basic styling and pages

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 from vocabulus import app
 from vocabulus.config import DEBUG
 
-print(DEBUG)
 if __name__ == '__main__':
 	app.run(debug=DEBUG)
--- vocabulus/routes.py
+++ vocabulus/routes.py
@@ -10,8 +10,8 @@ def index():
 # Error handling routes
 @app.errorhandler(404)
 def error_404(error):
-	return render_template("index.html"), 404
+	return render_template("error.html", error_code=404, error_message="Not Found"), 404
 
 @app.errorhandler(500)
 def error_500(error):
-	return render_template("index.html"), 500
+	return render_template("error.html", error_code=500, error_message="Internal Server Error"), 500
