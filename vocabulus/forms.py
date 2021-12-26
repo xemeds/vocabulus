64d63bac619609def0c0fba89c507289d3b8f24f
xemeds <xemeds@protonmail.com>
2021-12-26 21:04:02 +0300

    Added the word form

--- vocabulus/forms.py
+++ vocabulus/forms.py
@@ -1,3 +1,28 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField, ValidationError
 from wtforms.validators import DataRequired, Length, Optional
+from vocabulus.words import words
+
+# Validates a word
+def validate_word(form, field):
+	# Make sure the word is not too short or long
+	if len(field.data) < 1 or len(field.data) > 100:
+		return
+
+	# Remove the spaces in the word
+	field.data = field.data.replace(" ", "")
+
+	# Check if the word exists in the word list
+	try:
+		words[field.data]
+	except KeyError:
+		raise ValidationError("Word does not exists in the system")
+
+class WordForm(FlaskForm):
+	target_word = StringField(validators=[DataRequired(), Length(min=1, max=100, message="Invalid word length"), validate_word])
+
+	relation_word1 = StringField(validators=[DataRequired(), Length(min=1, max=100, message="Invalid word length"), validate_word])
+
+	relation_word2 = StringField(validators=[DataRequired(), Length(min=1, max=100, message="Invalid word length"), validate_word])
+
+	submit = SubmitField("Find")
--- vocabulus/routes.py
+++ vocabulus/routes.py
@@ -1,12 +1,24 @@
 from flask import render_template, redirect, url_for
 from vocabulus import app
 from vocabulus.words import words, closest_word
-# from vocabulus.forms import FORMS
+from vocabulus.forms import WordForm
 
 # Index route
 @app.route("/", methods=['GET', 'POST'])
 def index():
-	return render_template("index.html")
+	# Create a instance of the form
+	form = WordForm()
+
+	# If the form was valid
+	if form.validate_on_submit():
+		# TODO
+		# Return the index page with the form
+		return render_template("index.html", form=form)
+
+	# Else if the form was invalid or not submitted
+	else:
+		# Return the index page with the form
+		return render_template("index.html", form=form)
 
 # Error handling routes
 @app.errorhandler(404)
